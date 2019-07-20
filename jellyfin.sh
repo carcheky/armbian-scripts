6b6e661838c4284abda8ea5d3d6a2fce56f4daad
carcheky <carcheky@gmail.com>
2019-07-20 21:49:47 +0200

    Update jellyfin.sh

--- jellyfin.sh
+++ jellyfin.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # bash -c "$(curl -fsSL https://raw.githubusercontent.com/carcheky/armbian-scripts/master/jellyfin.sh)"
 
+
+
 sudo apt install apt-transport-https -y
 sudo add-apt-repository universe
 wget -O - https://repo.jellyfin.org/ubuntu/jellyfin_team.gpg.key | sudo apt-key add -
